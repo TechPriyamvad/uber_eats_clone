428ed6b320e37799e88bb8b00e1b4a088584a0d2
Technical Priyam <priyamvadofficial1@gmail.com>
2021-10-16 21:22:31 +0530

    updated app sdk to firebase

--- firebase.js
+++ firebase.js
@@ -1,12 +1,17 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsgb5Ri0DRK9gE0nJ0PYCh9BLByhbJatE",
-  authDomain: "rn-uber-eats-clone-yt.firebaseapp.com",
-  projectId: "rn-uber-eats-clone-yt",
-  storageBucket: "rn-uber-eats-clone-yt.appspot.com",
-  messagingSenderId: "791658363133",
-  appId: "1:791658363133:web:91e4fe1be6f3f23467a694",
+  apiKey: "AIzaSyDF8ASaG0raeQT07iqW8sovjZ4llZPxjew",
+
+  authDomain: "react-native-uber-eats-c-189e9.firebaseapp.com",
+
+  projectId: "react-native-uber-eats-c-189e9",
+
+  storageBucket: "react-native-uber-eats-c-189e9.appspot.com",
+
+  messagingSenderId: "829554972176",
+
+  appId: "1:829554972176:web:0937beb0d5879b296cdc95",
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
